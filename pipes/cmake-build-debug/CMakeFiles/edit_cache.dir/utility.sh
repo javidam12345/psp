1feeece41e0b1322a13f6951424b078db40dcc54
javidam12345 <javiaragoneses1@outlook.com>
2023-10-25 09:33:54 +0200

    a

---
+++ pipes/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/FX506/Desktop/psp/pipes/cmake-build-debug
+/cygdrive/c/Users/FX506/AppData/Local/JetBrains/CLion2023.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
---
+++ pipes/cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/FX506/Desktop/psp/pipes/cmake-build-debug
+/cygdrive/c/Users/FX506/AppData/Local/JetBrains/CLion2023.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
