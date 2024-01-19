a4bd5c387ba0788628ff99110f1cf5d44e0044ca
javidam12345 <javiaragoneses1@outlook.com>
2024-01-19 10:54:34 +0100

    test

--- refineria/src/Deposit.java
+++ refineria/src/Deposit.java
@@ -1,30 +1,35 @@
 public class Deposit {
     private double liters;
-    private boolean isCharging = false;
+    private boolean isFull = false;
 
     public synchronized double fillTankTruck() {
-        while (liters <= 0) {
+        while (!isFull) {
             try {
                 wait();
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
                 throw new RuntimeException(e);
             }
         }
         double toReturn = liters;
         liters = 0;
+        isFull = false;
         notifyAll();
         return toReturn;
     }
 
     public synchronized void producerFillsDeposit(double liters) {
+        while (isFull) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        }
         this.liters = liters;
         System.out.println("Producer fills the tank with " + liters);
+        isFull = true;
         notifyAll();
-        try {
-            wait();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 }
