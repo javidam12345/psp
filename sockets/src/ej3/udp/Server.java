dad35eb14373bed405780f1bdc5a8c8d2b4ad928
javidam12345 <javiaragoneses1@outlook.com>
2024-02-27 13:50:54 +0100

    test

--- sockets/src/ej3/udp/Server.java
+++ sockets/src/ej3/udp/Server.java
@@ -1,13 +1,15 @@
 package ej3.udp;
 
 import javax.xml.crypto.Data;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class Server {
     public static void main(String[] args) {
         try(DatagramSocket datagramSocket = new DatagramSocket()){
-
+            //DatagramPacket datagramPacket = new DatagramPacket();
+            //datagramSocket.send();
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
